a7afb66c12b3671fe6fbc3e82a8e6c7a56adc551
Alec Hsu <alechsu@Alecs-Laptop.local>
2025-06-04 16:04:35 -0700

    fix api doc urls

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -41,6 +41,6 @@ urlpatterns = [
     
     # API Schema documentation
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
