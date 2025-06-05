7399ea4fb3281e3f87514302136a5857d5ba7549
Alec Hsu <alechsu@Alecs-Laptop.local>
2025-06-05 16:13:32 -0700

    configure deployment settings

---
+++ config/deployment_settings.py
@@ -0,0 +1,52 @@
+import os
+import dj_database_url
+
+from .settings import *
+
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+DEBUG = False
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    # Added 'whitenoise.middleware.WhiteNoiseMiddleware' for render deployment
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# # CORS settings
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",  # React default port
+#     "http://127.0.0.1:3000",
+# ]
+
+STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STORAGES = {
+    "default":{
+        "BACKEND" : "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND" : "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+
+}
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default= os.environ.get('DATABASE_URL'), 
+        conn_max_age=600
+    )
+}
