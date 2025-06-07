2ebb11bf6b636669dedf90b73571ad0bc2affbcb
Alec Hsu <alechsu@mac.lan>
2025-06-06 21:43:40 -0700

    model update

--- snippets/models.py
+++ snippets/models.py
@@ -4,6 +4,7 @@ from pygments.styles import get_all_styles
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
+from django.contrib.auth import get_user_model
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
@@ -17,7 +18,7 @@ class Snippet(models.Model):
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
-    owner = models.ForeignKey('auth.user', related_name='snippets', on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(get_user_model(), related_name='snippets', on_delete=models.CASCADE, null=True)
     highlighted = models.TextField()
 
     class Meta:
