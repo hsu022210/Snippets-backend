277852396f1ce0cc68d433be1e176330e1d818ce
Alec Hsu <alechsu@Alecs-Laptop.local>
2025-06-09 17:39:38 -0700

    update admin snippet page

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Snippet
 
-# Register your models here.
-admin.site.register(Snippet)
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'language', 'created')
+    list_filter = ('owner', 'created')
+    search_fields = ('title', 'code')
+    ordering = ('-created',)
