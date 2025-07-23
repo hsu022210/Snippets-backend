34400cba26a4d007e8c1863250daba2c9f6b9284
Alec Hsu <hsu022210@gmail.com>
2025-07-22 19:11:09 -0700

    host

--- config/deployment_settings.py
+++ config/deployment_settings.py
@@ -3,8 +3,8 @@ import dj_database_url
 
 from .settings import *
 
-ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://' + "*"]
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
